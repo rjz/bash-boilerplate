f418204d3013f163a31774778cfc59d6c7e30c56
rjz <rj@rjzaworski.com>
2015-03-08 09:19:04 -0700

    In the beginning

---
+++ README.md
@@ -0,0 +1,18 @@
+# Bash boilerplate
+
+A starting point for command-line scripts--structure, environment, and
+facilities for logging and command line parsing.
+
+    $ ./script.sh
+    ./script.sh greet|dismiss name
+
+    $ ./script.sh greet McCoy
+    [2015-03-08T09:17:28-0700] - So then I says, 'Top o' the morning to ye, McCoy!'
+
+    $ ./script.sh fail hard
+    [2015-03-08T09:16:09-0700] - FATAL: 'I don't feel so well....'
+
+## License
+
+WTFPL
+
---
+++ script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Configurable variables
+SALUTATION="Top o' the morning to ye"
+VALEDICTION="Fare thee well"
+
+EXPECTEDARGC=2
+
+# Environmental variables
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Utility functions
+usage () {
+  echo "$0 greet|dismiss name"
+  exit 0
+}
+
+log () {
+  echo "[`date -Is`] - $1"
+}
+
+fatal () {
+  log "FATAL: '$1'"
+  exit 1
+}
+
+# User-defined functions
+say () {
+  log "So then I says, '$1'"
+}
+
+# Command line parsing
+if [ $# -lt $EXPECTEDARGC ]; then
+  usage
+fi
+
+case "$1" in
+
+  greet)
+    say "$SALUTATION, $2!"
+  ;;
+
+  dismiss)
+    say "$VALEDICTION, $2!"
+  ;;
+
+  fail)
+    fatal "I don't feel so well...."
+  ;;
+
+  *)
+    usage
+esac
+
